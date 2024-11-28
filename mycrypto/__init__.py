c6853a742c8951321424c3cad1d055d7355271cc
NICO9 <nmmendez9@hotmail.com>
2024-11-28 16:43:28 +0400

    feat: creadas variables de entorno y app lista para ejecutar

---
+++ mycrypto/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_prefixed_env()
+
+print('***** VARIABLES DE ENTORNO *****')
+print('DEBUG', app.config['DEBUG'])
+# print('APP', app.config['APP'])
+print('SECRET KEY', app.config['SECRET_KEY'])
